9e2a3c77ebbe14508bc91a7c72bf01c531cda1c0
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-10-21 22:27:08 +0100

    final exercise

--- challenges/programmer-humour/script.js
+++ challenges/programmer-humour/script.js
@@ -1,11 +1,23 @@
 let url = 'https://xkcd.now.sh/?comic=latest'
-let imageContainerEl = document.querySelector('.image-container')
-let newImageEl = document.querySelector('.new-image')
 
+function programmerHumour() {
+  fetch(url)
+		.then((response) => {
+			if (response.status > 199 && response.status < 300) {
+				return response.json();
+			} else {
+				throw new Error("Error ${response.status} ${response.statusText}");
+			}
+		})
+		.then((data) => {
+			let imageContainerEl = document.querySelector(".image-container");
+			let img = document.createElement("img");
+			img.src = data.img;
+			imageContainerEl.appendChild(img);
+		})
+		.catch((error) => console.log(error));
+}
 
-imageContainerEl.className = Image
+programmerHumour();
 
-fetch(url)
-.then((response) => response.json())
-.then((data) => console.log(data));
-imageContainerEl = <img src = "${data.meassage"/>
+// still need to add button when have time to change image 
